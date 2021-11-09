d52ceb900424118948ecdae85fdd747ca5172ace
Sheila <sheilaserranolozano@gmail.com>
2021-11-09 20:34:37 +0100

    Updates

---
+++ precache-manifest.a473e2592e964045cc5397162252b687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843199ea071456dc85fc3f52785d8a83",
+    "url": "/App_Diario_REACT_REDUX/index.html"
+  },
+  {
+    "revision": "07d4c8181c931c8fec43",
+    "url": "/App_Diario_REACT_REDUX/static/css/main.9c083248.chunk.css"
+  },
+  {
+    "revision": "e0e364d9fb4d9fa72bce",
+    "url": "/App_Diario_REACT_REDUX/static/js/2.9ef3e043.chunk.js"
+  },
+  {
+    "revision": "b71e64c63d6c8999bfdc8a62c15bd83e",
+    "url": "/App_Diario_REACT_REDUX/static/js/2.9ef3e043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d4c8181c931c8fec43",
+    "url": "/App_Diario_REACT_REDUX/static/js/main.7705a215.chunk.js"
+  },
+  {
+    "revision": "67163834ca0437246085",
+    "url": "/App_Diario_REACT_REDUX/static/js/runtime-main.cc952372.js"
+  }
+]);
